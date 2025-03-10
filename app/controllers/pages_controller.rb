00f00b9f87b41373c87a3f3583da1b30ae8bfb1d
Sarkraf PC <sarkraf.dev@gmail.com>
2025-03-10 01:34:16 +0100

    Fix import bundle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+
+
   def home
     @home_props = { name: current_user&.name || "Visiteur", authenticityToken: form_authenticity_token, currentUser: current_user }
   end
